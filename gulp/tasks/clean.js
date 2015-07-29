7c4f9400ab7b47d0b692dea14da3ef2ae6105a8b
insanehong <insanehong@gmail.com>
2015-07-30 00:09:57 +0900

    Update 2015-07-29T15:09:43.801Z

--- gulp/tasks/clean.js
+++
@@ -1,7 +0,0 @@
-var gulp            = require('gulp');
-var del             = require('del');
-var config          = require('../config').clean;
-
-module.exports = gulp.task(config.taskname, function (cb) {
-  del(config.target, cb);
-});
--- src/client/scripts/common.js
+++
@@ -1,7 +0,0 @@
-!(function($) {
-  'use strict';
-
-  $('#share-fb').click(function() { shareFb(); });
-  $('#share-tw').click(function() { shareTw(); });
-
-})(window.jQuery);
