4485594e25b991c091e1efb38d9ec7800c37cf17
daclouds <daclouds@gmail.com>
2015-10-22 18:12:20 +0900

    add register button

--- src/client/scripts/common.js
+++ src/client/scripts/common.js
@@ -12,17 +12,17 @@
 
   });
 
-  $('#sponsor-btn').on('click', function(e) {
-    var w = 800, h = 600,
-        l = (window.screen.width / 2) - ((800 / 2) + 10),
-        t = (window.screen.height / 2) - ((600 / 2) + 50);
-    var qs = "status=no,height=" + h + ",width=" + w + ",resizable=yes,left=" + l +
-      ",top=" + t + ",screenX=" + l + ",screenY=" + t +
-      ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no";
-
-    var popupUrl = 'sponsor.html';
-    window.open(popupUrl, 'play.node 2015', qs);
-  });
+  // $('#sponsor-btn').on('click', function(e) {
+  //   var w = 800, h = 600,
+  //       l = (window.screen.width / 2) - ((800 / 2) + 10),
+  //       t = (window.screen.height / 2) - ((600 / 2) + 50);
+  //   var qs = "status=no,height=" + h + ",width=" + w + ",resizable=yes,left=" + l +
+  //     ",top=" + t + ",screenX=" + l + ",screenY=" + t +
+  //     ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no";
+  //
+  //   var popupUrl = 'sponsor.html';
+  //   window.open(popupUrl, 'play.node 2015', qs);
+  // });
 
   $('#d-day').ready(function() {
     var now = new Date();
