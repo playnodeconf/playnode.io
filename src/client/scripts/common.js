56b2ca1360a75374dc6e1bc4ea3edd929bf01dff
insanehong <insanehong@gmail.com>
2015-10-26 08:14:58 +0900

    change to official site design

--- src/client/scripts/common.js
+++ src/client/scripts/common.js
@@ -12,24 +12,67 @@
 
   });
 
-  // $('#sponsor-btn').on('click', function(e) {
-  //   var w = 800, h = 600,
-  //       l = (window.screen.width / 2) - ((800 / 2) + 10),
-  //       t = (window.screen.height / 2) - ((600 / 2) + 50);
-  //   var qs = "status=no,height=" + h + ",width=" + w + ",resizable=yes,left=" + l +
-  //     ",top=" + t + ",screenX=" + l + ",screenY=" + t +
-  //     ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no";
-  //
-  //   var popupUrl = 'sponsor.html';
-  //   window.open(popupUrl, 'play.node 2015', qs);
-  // });
-
   $('#d-day').ready(function() {
     var now = new Date();
     var then = new Date("november 12,2015");
     var gap = then.getTime() - now.getTime();
     gap = Math.floor(gap / (1000 * 60 * 60 * 24));
     $('#d-day').text(' (d-' + gap + ')');
+  });
+
+  $('.top-navigation').on('click', 'a', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+      if (target.length) {
+        $('#top-naviation').removeClass('active');
+
+        $('html,body').animate({
+          scrollTop: target.offset().top - 70
+        }, 500);
+        return false;
+      }
+    }
+  });
+
+  $('.speakers').on('click', '[data-toggle="speakers"]', function(e) {
+    e.preventDefault();
+
+    $('body').removeClass('modal-open');
+
+    var target = $(this);
+    var name = target.data('name');
+
+    if(target.data('sns')) {
+      name = $('<a/>', {
+        target: '_blink',
+        href : target.data('sns'),
+        text: target.data('name')
+      });
+    }
+
+    $('#profileModalImg').attr('src', target.data('img'));
+    $('#profileModalName').html(name);
+    $('#profileModalCompany').text(target.data('company'));
+    $('#profileModalInfo').html(target.data('info'));
+
+    $('body').addClass('modal-open');
+
+  });
+
+  $('body').on('click', '.profile-modal-container', function(e) {
+    if($(e.target).parents('.profile-modal-container').length === 0) {
+      $('body').removeClass('modal-open');
+    }
+  });
+
+  $('.profile-modal-close-button button').on('click' ,function() {
+    $('body').removeClass('modal-open');
+  });
+
+  $('.navbar-toggle').on('click', function() {
+    $('#top-naviation').addClass('active');
   });
 
 })(window.jQuery);
