d925cbfbb86c51a05d010953df3995e61aea3f26
daclouds <daclouds@gmail.com>
2015-10-25 15:20:47 +0900

    remove duplicated scroll event

--- src/client/scripts/sticky.js
+++ src/client/scripts/sticky.js
@@ -30,31 +30,29 @@
 
     /* animation-element */
     var $window = $(window);
-    $window.on('scroll resize', check_if_in_view);
+    // $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
 
-    function check_if_in_view(event) {
-      var window_height = $window.height();
-      var window_top_position = $window.scrollTop();
-      var window_bottom_position = (window_top_position + window_height);
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
 
-      var $animation_elements = $('.animation-element');
+    var $animation_elements = $('.animation-element');
 
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
 
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-          (element_top_position <= window_bottom_position)) {
-          $element.addClass('in-view');
-        } else {
-          // $element.removeClass('in-view');
-        }
-      });
-    }
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        // $element.removeClass('in-view');
+      }
+    });
 
   };
 
