a56cc90f39bccc396dea00d623d03fd8cdd2f25c
Outsider <outsideris@gmail.com>
2016-11-03 01:24:35 +0900

    make first layout roughly

--- src/client/2016/js/init.js
+++ src/client/2016/js/init.js
@@ -1,8 +1,16 @@
-(function($){
-  $(function(){
+(function(){
+  // map
+  var map = new naver.maps.Map('map', {
+    center: new naver.maps.LatLng(37.504836, 127.027206),
+    scaleControl: true,
+    zoomControl: true,
+    mapTypeControl: true,
+    zoom: 12
+  });
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+  var marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.504836, 127.027206),
+    map: map
+  });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+})();
